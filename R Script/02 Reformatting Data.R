317c287c0a52fa99c4350cd74d68a2236a616c05
Jeffrey Jpse <jeffreyjose91@gmail.com>
2022-09-06 22:09:29 +1200

    Created a plot to showcase the top 15 videos by number of views

--- R Script/02 Reformatting Data.R
+++ R Script/02 Reformatting Data.R
@@ -21,3 +21,24 @@ video_final.df$ytPublishedDate <- as.Date(video_final.df$ytPublishedDateTime)
 video_final.df$ytPublishedDateTime <- gsub("T", " ", video_final.df$ytPublishedDateTime, fixed=TRUE)
 video_final.df$ytPublishedDateTime <- gsub("Z","",as.character(video_final.df$ytPublishedDateTime))
 video_final.df$ytPublishedTime <- format(as.POSIXct(video_final.df$ytPublishedDateTime), format = '%H:%M:%S')
+video_final.df$ytYearMonth <- format(video_final.df$ytPublishedDate, "%Y-%m")
+
+
+## Plots
+video_final.df %>%
+  group_by(ytChannelName, ytYearMonth) %>%
+  summarise(ytSumOfViews = sum(as.integer(ytViewsCount))) %>%
+  ggplot(aes(x = ytYearMonth, y = ytSumOfViews, group = ytChannelName, colour = ytChannelName)) + geom_line(size = 2) + geom_point() +
+  labs(x = "Year-Month", y = "Num. Views", title = "Number Of Viewers For Videos Released Each Year-Month") +
+  theme_light()
+
+## Top 10 videos with highest views
+video_final.df %>%
+  group_by(ytChannelName, ytTitle) %>%
+  summarise(ytViewsCount = as.integer(ytViewsCount), ytLikesCount = as.integer(ytLikesCount), ytCommentCount = as.integer(ytCommentCount)) %>%
+  arrange(desc(ytViewsCount)) %>%
+  slice(1:15) %>%
+  ggplot(aes(x = reorder(ytTitle, -ytViewsCount), y = ytViewsCount, group = ytChannelName, fill = ytChannelName)) + geom_bar(stat = "identity", colour = 'black') +
+  scale_x_discrete(guide = guide_axis(angle = 90)) +
+  labs(x = "Video Title", y = "Num. Views", title = "Top 15 Videos With The Highest Views", fill = "Channel Name:") + 
+  theme_light()
